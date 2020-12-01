e4971ba759ae24bbd9ec05571bdc6c4936f4e0bc
Chris <denver929@gmail.com>
2020-12-01 10:06:30 -0700

    testing git

---
+++ lib/brewery_finder/version.rb
@@ -0,0 +1,3 @@
+module BreweryFinder
+  VERSION = "0.1.0"
+end
---
+++ lib/environment.rb
@@ -0,0 +1,6 @@
+#require "brewery_finder/version"
+
+#module BreweryFinder
+ # class Error < StandardError; end
+  # Your code goes here...
+#end
